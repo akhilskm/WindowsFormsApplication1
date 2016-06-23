22ea878c8d68ed03cfa8ee728bfb66d404b1dc7c
Akhil Srivas <akhilskm94@outlook.com>
2016-06-23 17:12:25 +0530

    First Commit

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,33 +29,95 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 116);
+            this.button1.Location = new System.Drawing.Point(78, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 40);
+            this.button1.Size = new System.Drawing.Size(125, 26);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Click Me";
+            this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // num1
+            // 
+            this.num1.AccessibleName = "num1";
+            this.num1.Location = new System.Drawing.Point(78, 64);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(50, 20);
+            this.num1.TabIndex = 1;
+            this.num1.Text = "Num1";
+            this.num1.Enter += new System.EventHandler(this.num1_Enter);
+            // 
+            // num2
+            // 
+            this.num2.AccessibleName = "num2";
+            this.num2.Location = new System.Drawing.Point(153, 64);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(50, 20);
+            this.num2.TabIndex = 2;
+            this.num2.Text = "Num2";
+            this.num2.Enter += new System.EventHandler(this.num2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "+";
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.Location = new System.Drawing.Point(135, 169);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(0, 13);
+            this.answer.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Answer : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,14 +12,39 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Boolean a1,a2;
         public Form1()
         {
             InitializeComponent();
+            a1 = true;
+            a2 = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double n1,n2;
+            if (Double.TryParse(num1.Text, out n1)&& Double.TryParse(num2.Text, out n2))
+                answer.Text = (n1+n2).ToString();
+            else
+                answer.Text = "Invalid Number";
+        }
 
+        private void num2_Enter(object sender, EventArgs e)
+        {
+            if (a2)
+            {
+                num2.Text = "";
+                a2 = false;
+            }
+        }
+
+        private void num1_Enter(object sender, EventArgs e)
+        {
+            if (a1)
+            {
+                num1.Text = "";
+                a1 = false;
+            }
         }
     }
 }
